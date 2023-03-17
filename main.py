e6c0a52baccbe0e5f90ad22f67ec459b26968624
wojciech <wwadamski@gmail.com>
2023-03-18 00:49:05 +0100

    final

--- main.py
+++ main.py
@@ -6,11 +6,22 @@ from kivy.vector import Vector
 
 class PongGame(Widget):
     ball = ObjectProperty(None)
+
+    def serve_ball(self):
+        self.ball.center = self.center
+        self.ball.velocity = Vector(4, 0).rotate(randint(0, 360))
+
     def update(self, dt):
         self.ball.move()
+
+        # bounce off top and bottom
         if (self.ball.y < 0) or (self.ball.top > self.height):
             self.ball.velocity_y *= -1
-        if (self.ball.x <0 ) or (self.ball.right > self.width)
+
+        # bounce off left and right
+        if (self.ball.x < 0) or (self.ball.right > self.width):
+            self.ball.velocity_x *= -1
+
 
 class PongApp(App):
     def build(self):
